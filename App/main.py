26497c4cb558dc99d6bb8515606f55150cfb7f6c
NikLessey <nalessey2@gmail.com>
2023-02-24 22:52:12 -0400

    Update 24.02.2023

--- App/main.py
+++ App/main.py
@@ -1,10 +1,31 @@
 import os
 import requests
-from flask import Flask, redirect, render_template, request, jsonify, send_from_directory, flash, url_for
+from flask import (
+            Flask, 
+            redirect, 
+            render_template, 
+            request, 
+            jsonify, 
+            send_from_directory, 
+            flash, 
+            url_for
+)
+
 from flask_cors import CORS
 from sqlalchemy.exc import OperationalError
-from models import db, get_migrate, create_db
-from models import Test, Post, Board, User
+from models import (
+            db, 
+            get_migrate, 
+            create_db
+)
+
+from models import (
+            Test, 
+            Post, 
+            Board, 
+            User
+)
+
 
 def create_app():
   app = Flask(__name__, static_url_path='/static')
@@ -13,7 +34,7 @@ def create_app():
   app.config['TEMPLATE_AUTO_RELOAD'] = True
   app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
   app.config['DEBUG'] = True
-  app.config['SECRET_KEY'] = 'MySecretKey'
+  app.config['SECRET_KEY'] = 'password'
   app.config['PREFERRED_URL_SCHEME'] = 'https'
   create_db(app)
   app.app_context().push()
